211d0dc1b4f0d1c6c95893447149ffbbafc14650
Ramakrishnan Aadhali <014ramakrishnanaa@gmail.com>
2026-03-12 17:32:18 +0530

    Refactor UserRepository.java for improved readability

--- src/main/java/com/example/student_management/repository/UserRepository.java
+++ src/main/java/com/example/student_management/repository/UserRepository.java
@@ -1,10 +1,6 @@
 package com.example.student_management.repository;
-
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.student_management.entity.User;
 
 public interface UserRepository
